5bee61bfa50d2095132eb25a37a05ab0249da7cd
MIMILICA0330 <it09016760153@gmail.com>
2026-08-21 06:50:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aws-interview-coach-static-v11";
+const CACHE_NAME = "aws-interview-coach-static-v12";
 const AUDIO_ORIGIN = "https://interview-coach-tts.it09016760153.workers.dev";
 const ASSETS = ["./", "./index.html", "./styles.css", "./fixes.css", "./content.js", "./translations.js", "./content-expanded.js", "./tts-config.js", "./app.js", "./audio-manifest.json", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (event) => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
