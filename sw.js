528108673dbb8da3602ec01eabf821b405142216
MIMILICA0330 <it09016760153@gmail.com>
2026-08-21 23:20:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "aws-interview-coach-static-v12";
+const CACHE_NAME = "aws-interview-coach-static-v13";
 const AUDIO_ORIGIN = "https://interview-coach-tts.it09016760153.workers.dev";
-const ASSETS = ["./", "./index.html", "./styles.css", "./fixes.css", "./content.js", "./translations.js", "./content-expanded.js", "./tts-config.js", "./app.js", "./audio-manifest.json", "./manifest.webmanifest", "./icon.svg"];
+const ASSETS = ["./", "./index.html", "./styles.css", "./fixes.css", "./content.js", "./translations.js", "./content-expanded.js", "./content-refresh.js", "./tts-config.js", "./app.js", "./access.js", "./audio-manifest.json", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (event) => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
